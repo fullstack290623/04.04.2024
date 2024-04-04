89dc9e28fc7ce7ef2862110f8c9f93ad896ff632
fullstack290623 <fullstack290623@gmail.com>
2024-04-04 18:29:47 +0300

    .

---
+++ app.js
@@ -0,0 +1,8 @@
+const dbURI = "mongodb://root:rootpassword@localhost:27022/node-auth?authSource=admin";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    console.log(result.connection)    
+    //app.listen(3000, () => console.log(`Listening to port ${3000}`))
+  })
+  .catch((err) => console.log(err));
