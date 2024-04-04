0c6207814102e5feaf3f1b046b72a4f2647bae43
fullstack290623 <fullstack290623@gmail.com>
2024-04-04 18:31:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const dbURI = "mongodb://root:rootpassword@localhost:27022/node-auth?authSource=
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => {
-    console.log(result.connection)    
+    console.log('Mongo connected ...');
+    console.log(result.connection._connectionString)    
     //app.listen(3000, () => console.log(`Listening to port ${3000}`))
   })
   .catch((err) => console.log(err));
